7d2600510af5a1f06eb495929fd450aef592e444
konghy <konghy@pvc123.com>
2019-10-28 11:40:44 +0800

    feat:初步搭建

--- app/Http/Controllers/Blog/IndexController.php
+++ app/Http/Controllers/Blog/IndexController.php
@@ -5,7 +5,15 @@ namespace App\Http\Controllers\Blog;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
-class IndexController extends Controller
+/**
+ * Class IndexController
+ * @package App\Http\Controllers\Blog
+ * 博客首页
+ */
+class IndexController extends BaseController
 {
-    //
+
+    public function index() {
+
+    }
 }
